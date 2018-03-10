a085c42a29527dcc95a8cb680c180f5b99e1ab1f
Marios Sotiropoulos <marsotir@gmail.com>
2018-03-10 17:25:30 +0200

    login

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,60 +1,49 @@
-var userId = "5aa3b6be96489c3014187f7a";
-var token = "8kjvVOSYbGfNhEOrUj3ge63RQNobRCVhpAjf7gOJEQBvjy1bhXpSNmBzlrDtLMe2";
+var myStorage = window.localStorage;
 
 var httpreq = axios.create({
 	baseURL : 'http://todo.socialapps.eu/api',
 	headers: {
-		common: {
+		/*common: {
 			'Authorization': token
-		}
+		}*/
 	}
 })
 
 var ToDoList = (function () {
-	var myStorage = window.localStorage;
 	var todosList = [];
 	var doneList = [];
+	var _userId;
 
-	function _createToDoList(el) {
-		var tpl = '<li class="list-group-item border-none added"><label><input type="checkbox" onclick="ToDoList.addToDone(this)" name="item[]" value="'+el.title+'">'+el.title+'</label></li>';
+	function _createToDoList(key, el) {
+		var tpl = '<li class="list-group-item border-none added"><label><input type="checkbox" onclick="ToDoList.addToDone(this)" name="item[]" value="'+key+'">'+el.title+'</label></li>';
   		document.getElementById('todolist').insertAdjacentHTML('beforeend', tpl)
   		var txt  = (todosList.length == 1)? ' item left' : ' items left';
   		document.getElementById('itemsLeft').innerHTML = todosList.length + txt;
 	}
 	
-	function _createDoneList(el) {
+	function _createDoneList(key, el) {
 		var title = (el.title)? el.title : el;
-		var tpl = '<li class="list-group-item removed"><del>'+title+'</del> <span onclick="ToDoList.remove(this)" data-title="'+title+'" class="btn btn-default btn-sm glyphicon glyphicon-remove"></span></li>';
+		var tpl = '<li class="list-group-item removed"><del>'+title+'</del> <span onclick="ToDoList.remove(this)" data-for="'+key+'" class="btn btn-default btn-sm glyphicon glyphicon-remove"></span></li>';
   		document.getElementById('donelist').insertAdjacentHTML('beforeend', tpl)
 	}
 	
-	function _removeFromToDo(el) {
-		var pos = _findItemPosition(todosList, "title", el);
-		httpreq.patch('todos/'+todosList[pos].id,{
+	function _removeFromToDo(pos, el) {
+		httpreq.patch('todos/'+el.id,{
 			status: false
 		})
-		doneList.push(todosList[pos]);
-		todosList.splice(pos, 1);
+		doneList.push(el);
 		var txt  = (todosList.length == 1)? ' item left' : ' items left';
   		document.getElementById('itemsLeft').innerHTML = todosList.length + txt;
 	}
-
-	function _findItemPosition (items, attribute, value) {
-	 	for (var i = 0; i < items.length; i++) {
-	    	if (items[i][attribute] === value) {
-	      		return i;
-	    	}
-	  	}
-	  	return null;
-	}
 	
-	function init() {
-		httpreq.get('/users/'+userId+'/todos?filter=%7B%22where%22%3A%7B%22status%22%3Atrue%7D%7D')
+	function init(userId) {
+		_userId = userId;
+		httpreq.get('/users/'+_userId+'/todos?filter=%7B%22where%22%3A%7B%22status%22%3Atrue%7D%7D')
 		  .then(function (response) {
 		    todosList = response.data;
 		    
 		    for (key in todosList) {
-				_createToDoList(todosList[key]);
+				_createToDoList(key, todosList[key]);
 	  		}
 	  		
 	  		var txt  = (todosList.length == 1)? ' item left' : ' items left';
@@ -65,12 +54,12 @@ var ToDoList = (function () {
 		  });
 
 
-		httpreq.get('/users/'+userId+'/todos?filter=%7B%22where%22%3A%7B%22status%22%3Afalse%7D%7D')
+		httpreq.get('/users/'+_userId+'/todos?filter=%7B%22where%22%3A%7B%22status%22%3Afalse%7D%7D')
 		  .then(function (response) {
 		    doneList = response.data;
 
 		    for (key in doneList) {
-				_createDoneList(doneList[key]);
+				_createDoneList(key, doneList[key]);
 	  		}
 
 		  })
@@ -82,14 +71,14 @@ var ToDoList = (function () {
 	
 	function addToDo(el) {
 	    if(el && el.length > 0){
-	    	httpreq.post('/users/'+userId+'/todos', {
-			    userId: userId,
+	    	httpreq.post('/users/'+_userId+'/todos', {
+			    userId: _userId,
 			    title: el,
 			    status: true
 			  })
 			  .then(function (response) {
 			  	todosList.push(response.data);
-			    _createToDoList(response.data);
+			    _createToDoList(todosList.length-1, response.data);
 			  })
 			  .catch(function (error) {
 			    console.log(error);
@@ -99,28 +88,27 @@ var ToDoList = (function () {
 	
 	function addToDone(el) {
 		if(el.checked){
-			_removeFromToDo(el.value);
-			_createDoneList(el.value);
+			_removeFromToDo(el.value, todosList[el.value]);
+			_createDoneList(doneList.length-1, todosList[el.value]);
 			el.parentElement.parentElement.remove();
 		}
 	}
 	
 	function allRead(el) {
 		var added = document.getElementsByClassName("added");
+
 		do {
-			_createDoneList(todosList[0].title);
-			_removeFromToDo(todosList[0].title);
+			_createDoneList(doneList.length, todosList[added[0].firstChild.firstChild.value].title);
+			_removeFromToDo(added[0].firstChild.firstChild.value, todosList[added[0].firstChild.firstChild.value]);
 		   added[0].remove();
 		} while (added.length > 0);
 		
 		todosList.length = 0;
-		myStorage.removeItem('todos');
   		document.getElementById('itemsLeft').innerHTML = '0 items left';
 	}
 	
 	function remove(el) {
-		var pos = _findItemPosition(doneList, "title", el.dataset.title);
-		httpreq.delete('todos/'+doneList[pos].id,{
+		httpreq.delete('todos/'+doneList[el.dataset.for].id,{
 			status: false
 		})
 		var removed = document.getElementsByClassName("removed");
@@ -140,8 +128,56 @@ var ToDoList = (function () {
 	}
 })();
 
+
+var Login = (function () {
+
+	function _toggleViews() {
+		document.getElementById('form').classList.add("hidden");
+		document.getElementById('lists').classList.remove('hidden')
+	}
+
+	function _doLogin(user, pass) {
+		httpreq.post('/users/login', {
+				email: user,
+				password: pass
+			})
+		  	.then(function (response) {
+		  		console.log('~~~~login', response)
+		  		if(response.status === 200){
+		  			var user = {'userId': response.data.userId, 'token': response.data.id};
+		  			myStorage.setItem('user', JSON.stringify(user));
+					httpreq.defaults.headers.common['Authorization'] = response.data.id;
+			  		ToDoList.init(response.data.userId);
+			  		_toggleViews();
+		  		}
+		  	})
+		  	.catch(function (error) {
+		    	console.log(error);
+		  	});
+	}
+
+	function init(){
+		if(myStorage.getItem('user')){
+			var user = JSON.parse(myStorage.getItem('user'));
+			httpreq.defaults.headers.common['Authorization'] = user.token;
+			ToDoList.init(user.userId);
+			_toggleViews();
+		}
+	}
+	
+	function doLogin(el) {
+		_doLogin(document.getElementById('email').value, document.getElementById('pass').value);
+	}
+	
+	return {
+	    init: init,
+	    doLogin: doLogin
+	}
+})();
+
+
 (function() {
-	ToDoList.init();
+	Login.init();
 })()
 
 document.addEventListener('keypress', function(e) {
@@ -154,4 +190,8 @@ document.addEventListener('keypress', function(e) {
 
 document.getElementById("allRead").addEventListener("click", function() {
     ToDoList.allRead();
+});
+
+document.getElementById("login").addEventListener("click", function() {
+    Login.doLogin();
 });
